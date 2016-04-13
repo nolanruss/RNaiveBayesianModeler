8cd22c55b51b425ed1e5e2a7b9234b0203bec8a7
Nolan Thompson <nolanruss@gmail.com>
2016-04-13 15:32:14 -0700

    Added function EegFFT to perform fft on all data in a frame

--- eegRDataProcessing.R
+++ eegRDataProcessing.R
@@ -1,6 +1,10 @@
 # Import the raw data. Class: data.frame
-Flower_Gray_Combined_Data <- read.csv("~/Documents/School/CSC_511_AI/Research_Proj/repository of eeg/ERPPaired/Flower_Gray_Combined_Data.csv", header=FALSE)
+Flower_Gray_Combined_Data <- read.csv("~/Documents/School/CSC_511_AI/Research_Proj/repository of eeg/ERPPaired/Flower_Gray_Combined_Data.csv", header=TRUE)
 colnames(Flower_Gray_Combined_Data) <- c("Type", "Subject", "Timepoint", paste0("El", seq(1:25))) # Set column names.
+
+# Order the data by subject, where each subject contains a data.frame of timepoint x electrode matrices.
+combinedDataBySubject <- data.frame(x=1:34)
+combinedDataBySubject <- combinedDataBySubject[,FALSE]
 
 # Extract all unique subject IDs.  Class: character array.
 subjectIDs <- levels(Flower_Gray_Combined_Data[,2])
@@ -42,6 +46,7 @@ binCreator <- function(df, subjMat){
   binFrame # Return the binFrame
 }
 
+
 binFrequency <- function(df, binF, subjMat){
   initialFrame <- data.frame(x=1:33)
   frequencyFrame <- initialFrame[,FALSE] # Create the frequency frame
@@ -51,7 +56,7 @@ binFrequency <- function(df, binF, subjMat){
     
     # Loop through all electrodes.
     for(j in 1:25){
-       # Loop through all elements.
+       # Loop through all bins, add the count of values < the bin value to the frequencyFrame.
        for(k in 1:33){
          binArray[k] <- count()
        }
@@ -67,3 +72,37 @@ flowerOnlyData <- Flower_Gray_Combined_Data[flowerRows,]
 grayRows <- Flower_Gray_Combined_Data[,1] == "gray"
 grayOnlyData <- Flower_Gray_Combined_Data[grayRows,]
 
+## Slow Discrete Fourier Transform (DFT) - e.g., for checking the formula
+fft0 <- function(z, inverse=FALSE) {
+  n <- length(z)
+  if(n == 0) return(z)
+  k <- 0:(n-1)
+  ff <- (if(inverse) 1 else -1) * 2*pi * 1i * k/n
+  vapply(1:n, function(h) sum(z * exp(ff*(h-1))), complex(1))
+}
+
+relD <- function(x,y) 2* abs(x - y) / abs(x + y)
+n <- 2^8
+z <- complex(n, rnorm(n), rnorm(n))
+
+# Performs Full Fourier Transform on each set of electrodes for each subject.
+# The function accepts a matrix of subject IDs (subjIDs), and data.frame (df).
+EegFFT <- function(df, subjIDs){
+    initialFrame <- data.frame(x=1:33)
+    
+    # frequencyFrame stores all fft data in a 256 x (34*25) data frame.
+    # 
+    frequencyFrame <- initialFrame[,FALSE] # Create the frequency frame
+  
+    for(i in 1:length(subjIDs)){
+        for(j in 4:28){
+            # create a matrix of fft values for 1 electrode, 1 subject.
+            fftArray <- matrix(fft(df[df[,2]==subjIDs[i],j]))
+            colnames(binArray) <- paste0(subjMat[i],"-El",(j-3)) # Name the matrix columns.
+            # Store the frequencies in the frequencyFrame.
+            frequencyFrame <- cbind(frequencyFrame, fftArray)
+        }
+    }
+    row.names(frequencyFrame) = paste0(1:256)
+    frequencyFrame # Return the binFrame
+}
